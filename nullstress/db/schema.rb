b26116710ef050ed848dcb2c3e159487e9b01d92
shey <shey@shey.ca>
2025-07-06 20:31:19 -0400

    pings model

--- nullstress/db/schema.rb
+++ nullstress/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_07_000549) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_07_002831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "pings", force: :cascade do |t|
+    t.bigint "sensor_id", null: false
+    t.integer "status_code"
+    t.float "response_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_pings_on_created_at"
+    t.index ["sensor_id", "created_at"], name: "index_pings_on_sensor_id_and_created_at"
+    t.index ["sensor_id", "status_code"], name: "index_pings_on_sensor_id_and_status_code"
+    t.index ["sensor_id"], name: "index_pings_on_sensor_id"
+  end
 
   create_table "sensors", force: :cascade do |t|
     t.string "name"
@@ -22,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_07_000549) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "pings", "sensors"
 end
